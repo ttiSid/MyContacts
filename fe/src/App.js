127032a3f25cf218aa4f16ca5c8536d7a4b0fd0f
Sidney Sudatti <s02devmail@protonmail.com>
2024-01-03 15:56:19 -0300

    Feature: editorconfig && ESLint config

--- fe/src/App.js
+++ fe/src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-asdasdasd        </p>
+          asdasdasd
+          {' '}
+
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- fe/src/index.js
+++ fe/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
   document.getElementById('root'),
 );
-
